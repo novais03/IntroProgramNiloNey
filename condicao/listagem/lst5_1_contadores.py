9d204c04328036cba5fb7f11d31d2aa29b5aff28
anselmonovais <anselmonnvix@yahoo.com>
2019-11-12 13:54:41 -0200

    exemplo de uso de contadores

---
+++ condicao/listagem/lst5_1_contadores.py
@@ -0,0 +1,10 @@
+"""
+Imprima os números inteiros entre 1 e 1 número digitado pelo usuário
+"""
+
+fim = int(input('Digite o último número a imprimir: '))
+x = 1
+
+while x <= fim:
+    print(x)
+    x = x + 1
