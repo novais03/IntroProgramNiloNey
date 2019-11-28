2f64396b4ccc477407238b1df466f9764fb77148
anselmonovais <anselmonnvix@yahoo.com>
2019-11-28 16:09:13 -0300

    Usando o enumerate

---
+++ C06listas/enumerate.py
@@ -0,0 +1,28 @@
+"""
+enumerate()
+
+ - Retorna o índice entre colcchetes e o valos à sua direita
+
+ # Exemplo
+
+# retornando o índice em o uso do enumerate
+for e in L:
+    print("[%d] %d" % (x, e))
+    x += 1
+# resultado
+
+[3] 5
+[4] 9
+[5] 13
+
+
+# Listagem 6.32 - Impressão índice usando enumerate
+l = [5, 9, 13]
+for x, e in enumerate(L):
+    print("[%d] %d" % (x, e))
+# Resultado
+
+[0] 5
+[1] 9
+[2] 13
+"""
