0da3ccd644700e3eea88b2f3dedeb6328b6ef3b4
anselmo <anselmonnvix@yahoo.com>
2019-12-03 01:40:06 -0300

    Ordenando listas

---
+++ c06listas/ordenacao.py
@@ -0,0 +1,21 @@
+"""
+Ordenando uma lista
+"""
+
+lista = [7, 4, 3, 12, 8]
+fim = 5
+while fim > 1:
+    trocou = False
+    x = 0
+    while x < (fim -1):
+        if lista[x] > lista[x+1]:
+            trocou =True
+            temp = lista[x]
+            lista[x] = lista[x+1]
+            lista[x+1] = temp
+        x += 1
+    if not trocou:
+        break
+    fim -= 1
+for e in lista:
+    print(e)
